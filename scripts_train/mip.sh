da378d3824441d8d78a8fdc287d3c9f2a9b4c56b
Zongqi HE <tangdi0110@gmail.com>
2025-02-19 16:05:39 +0800

    update

--- scripts_train/mip.sh
+++ scripts_train/mip.sh
@@ -1,4 +1,4 @@
 for SCENE in bonsai counter garden kitchen room stump bicycle
 do
-  CUDA_VISIBLE_DEVICES=0 python train.py --source_path  /home/data1/mipnerf360/$SCENE --model_path output_mip/output_mip_1/$SCENE --eval --n_views 24 --D 0.1 --W 0.25 --N 0.1
+  CUDA_VISIBLE_DEVICES=0 python train.py --source_path  /home/data/mipnerf360/$SCENE --model_path output/mip360/$SCENE --eval --n_views 24 --D 0.1 --W 0.25 --N 0.1
 done
